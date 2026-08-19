ab7ba7f5e98741f8f340f8f83e03afc80ff4ca0b
Pedro Vieira <pedrovieirafdev@gmail.com>
2026-08-19 17:15:06 -0300

    test: verify Spring application context

---
+++ src/test/java/com/pedro/ledger/LedgerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.pedro.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
